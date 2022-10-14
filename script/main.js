f3e68edb8fcb939df2cfe831c6b4ea7578e93ca8
Matheus-Cegatti <cegattimatheus@gmail.com>
2022-10-14 15:46:13 -0300

    Colocando o "0" quando horas/minutos/segundos for menor que 10

--- script/main.js
+++ script/main.js
@@ -8,7 +8,10 @@ const relogio = setInterval(function relogioFuncionando(){
     let minuto = dataAtual.getMinutes();
     let segundo = dataAtual.getSeconds();
 
-
+    if(hora < 10) hora = "0" + hora;
+    if(minuto < 10) minuto = "0" + minuto;
+    if(segundo < 10) segundo = "0" + segundo;
+    
     horas.textContent = hora
     minutos.textContent = minuto
     segundos.textContent = segundo
